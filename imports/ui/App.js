189f71cc80fd23915d492ee4c47bf79039c0f5ef
Povilas Jurcys <povilas@samesystem.com>
2018-09-06 08:18:30 +0300

    WIP

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
 
+import { Tasks } from '../api/tasks.js';
 import Task from './Task.js';
 
 // App component - represents the whole app
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
+class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
   renderTasks() {
-    return this.getTasks().map((task) => (
+    return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -21,8 +30,17 @@ export default class App extends Component {
   render() {
     return (
       <div className="container">
+
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
 
         <ul>
@@ -32,3 +50,10 @@ export default class App extends Component {
     );
   }
 }
+
+
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+})(App);
